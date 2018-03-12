7b309258ed7b78e0069e10c415a6022ee09263af
Evan <evan.st.burton@gmail.com>
2018-03-11 20:11:43 -0700

    Reindexing
    Reindexed to 1-based enumeration. Seems to have fixed some problems,
    correctly computed the deficiency of a network.

--- min_deficiency.py
+++ min_deficiency.py
@@ -8,6 +8,7 @@ from crnpy.crn import CRN, from_react_strings
 from pulp import *
 from itertools import product
 import numpy as np
+from pulp.solvers import CPLEX
 
 # Want to compute the minimal deficiency possible over all linearly conjugate
 # networks to crn that obey eps <= [A]_ij <= ubound.
@@ -23,42 +24,44 @@ def min_deficiency(crn, eps):
     # Convert the sympy matrix to numpy float64 matrix and compute rank
     s = np.linalg.matrix_rank(np.array(crn.stoich_matrix).astype(np.float64))
 
+    col_range = range(1, m+1)
+    row_range = range(1, n+1)
+    part_range = range(1, m-s+1)
+
     prob = LpProblem("Minimal Deficiency", LpMinimize)
     
     # Get a list of all off-diagonal entries to use later to greatly
     # simplify loops
-    off_diag = [(i, j) for i,j in product(range(m), repeat=2) if i != j]
+    off_diag = [(i, j) for i,j in product(col_range, repeat=2) if i != j]
     
     # Decision variables for matrix A, only need off-diagonal since A
     # has zero-sum columns
-    A = LpVariable.dicts("A", [(i, j) for i,j in product(range(m), repeat=2)])
+    A = LpVariable.dicts("A", [(i, j) for i,j in product(col_range, repeat=2)])
     
     # Decision variables for the diagonal of T
-    T = LpVariable.dicts("T", range(n), eps, 1/eps)
+    T = LpVariable.dicts("T", row_range, eps, 1/eps)
     
     # Kernel variables
     Phi = LpVariable.dicts("Phi", off_diag)
     
     # Binary variables
-    theta = LpVariable.dicts("theta", range(m-s), 0, 1, "Integer")
-    gamma = LpVariable.dicts("gamma", [(i,j) for i in range(m) for j in range(m-s)], 0, 1, "Integer")
+    theta = LpVariable.dicts("theta", part_range, 0, 1, "Integer")
+    gamma = LpVariable.dicts("gamma", [(i,j) for i in col_range for j in part_range], 0, 1, "Integer")
     
     # Objective function: minimize -sum theta[k] for k = 1..partitions
-    prob += lpSum( theta[k] for k in range(m-s) )
+    prob += m-s-lpSum( theta[k] for k in part_range )
     
     ########### Constraints ###########
-    
-    prob += T[0] == 1
     
     # Y*A = T*M
     for i in range(n):
         for j in range(m):
-            prob += lpSum(( Y[i, k]*A[k, j] for k in range(m))) == T[i]*M[i, j]
+            prob += lpSum( Y[i, k]*A[k+1, j+1] for k in range(m) ) == T[i+1]*M[i, j]
     
 
     # A has zero-sum columns
-    for j in range(m):
-        prob += lpSum((A[i,j] for i in range(n))) == 0
+    for j in col_range:
+        prob += lpSum( A[i,j] for i in row_range ) == 0
     
     # Off-diagonal constraints on A
     for (i,j) in off_diag:
@@ -66,51 +69,58 @@ def min_deficiency(crn, eps):
         prob += A[i, j] <= 1/eps
     
     # Diagonal entries must be nonpositive
-    for i in range(m):
+    for i in col_range:
         prob += A[i, i] <= 0 
     
     # Partitioning
-    for i in range(m):
-        prob += lpSum((gamma[i, k] for k in range(m-s))) == 1
+    for i in col_range:
+        prob += lpSum((gamma[i, k] for k in part_range)) == 1
         
         # Uniqueness constraint
-        for k in range(m-s):
+        for k in part_range:
             if k <= i:
-                prob += lpSum( (gamma[j, k] for j in range(i-1)) ) >= lpSum( (gamma[i, L] for L in range(k+1, m-s)) )
+                prob += lpSum( gamma[j, k] for j in range(1, i) ) >= lpSum( gamma[i, L] for L in range(k+1, m-s+1) )
         
-    for k in range(m-s):
-        prob += lpSum(( gamma[i, k] for i in range(m) )) >= eps*theta[k]
-        prob += lpSum(( gamma[i, k] for i in range(m) )) <= 1/eps*theta[k]
+    for k in part_range:
+        prob += lpSum( gamma[i, k] for i in col_range ) >= eps*theta[k]
+        prob += lpSum( gamma[i, k] for i in col_range ) <= 1/eps*theta[k]
         
     # Kernel constraints
     for (i,j) in off_diag:
-        prob += lpSum(( Phi[i, L] - Phi[L, i] for L in range(i-1) if L != i)) == 0
+        prob += lpSum(( Phi[i, L] - Phi[L, i] for L in range(1, i) if L != i)) == 0
     
-        for k in range(m-s):
+        for k in part_range:
             prob += eps*Phi[i,j] <= (gamma[i, k] - gamma[j, k] + 1)
             prob += Phi[i, j] >= eps*A[i, j]
             prob += Phi[i, j] <= 1/eps*A[i, j]
            
-    prob.solve() 
+    status = prob.solve(solver=CPLEX()) 
 
+    #print(prob)
+
+    print(status)
+    
     # Get solutions to problem
     gammasol = np.ndarray((m, m-s))
     Tsol = np.zeros((n, n))
     Asol = np.ndarray((m,m))
     
-    for i in range(m):
-        for j in range(m):
-            Asol[i, j] = value(A[i, j])
-        for k in range(m-s):
-            gammasol[i, k] = value(gamma[i, k])
+    for i in col_range:
+        for j in row_range:
+            Asol[i-1, j-1] = value(A[i, j])
+        for k in part_range:
+            gammasol[i-1, k-1] = value(gamma[i, k])
     
-    for i in range(n):
-        Tsol[i, i] = value(T[i])
+    for i in row_range:
+        Tsol[i-1, i-1] = value(T[i])
     
     print("A =", Asol)
     print("gamma = ", gammasol)
     print("T = ", Tsol)
+    print("deficiency = ", value(prob.objective))
 
-crn = from_react_strings(["0 ->(1) A1", "A1 ->(1) 0", "A1 ->(1) 2 A1", "2 A1 ->(1) A1", "A1 ->(1) A2 + A3", "A2 + A3 ->(2) A1", "A2 + A3 ->(1) 2 A2", "2 A2 ->(4) A2 + A3", "X2 + X3 ->(1) 2 X3", "2 X3 ->(1) X2 + X3"])
+#crn = from_react_strings(["0 ->(1) A1", "A1 ->(1) 0", "A1 ->(1) 2 A1", "2 A1 ->(1) A1", "A1 ->(1) A2 + A3", "A2 + A3 ->(2) A1", "A2 + A3 ->(1) 2 A2", "2 A2 ->(4) A2 + A3", "X2 + X3 ->(1) 2 X3", "2 X3 ->(1) X2 + X3"])
+crn = from_react_strings(["2 T100 (1)<->(1) 2 T001", "2 T100 (1)<->(1) 2 T010",
+                          "2 T001 (1)<->(1) 2 T010", "T100 + T001 (1)<->(1) T100 + T010", "T100 + T001 (1)<->(1) T010 + T001", "T100 + T010 (1)<->(1) T010 + T001"])
 
-min_deficiency(crn, 0.333)
+min_deficiency(crn, 0.01)
